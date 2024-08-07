d423a13c8301506b0207edb81b78732a916f7fa9
Arthur Melo <53615432+arthurmelo777@users.noreply.github.com>
2024-08-07 14:45:35 +0000

    gera artefato

---
+++ artefato/src/main/java/br/edu/ifrn/artefato/App.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.artefato;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
